af6ae13f9f712db65ce7ea67e0ffc61cdfb96c93
Destaritgithub <alexdesse@gmail.com>
2023-05-16 00:22:12 -0700

    initial commit

--- backend/Server.js
+++ backend/Server.js
@@ -1,1 +1,22 @@
-console.log('server is running');
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 8000;
+
+
+// connect to database
+connectDB()
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.get('/', (req, res) => {
+  res.status(204).json({ message: 'Welcome to the Support ' });
+});
+// Routes
+app.use('/api/users', require('./routes/UserRoutes'));
+app.use(errorHandler);
